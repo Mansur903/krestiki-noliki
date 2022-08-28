bc64462dafe951fc8144905bc64238dc8e846017
Mansur <mansur98@yandex.ru>
2022-08-28 18:04:44 +0300

    next update

--- src/App.jsx
+++ src/App.jsx
@@ -18,18 +18,27 @@ function App() {
     },
     step: 'prepareFirstPlayer',
     fieldSize: 3,
+    gameProgress: {
+      first: [],
+      second: [],
+      currentPlayer: '',
+    },
   });
 
+  const { first } = state.gameProgress;
+  const { second } = state.gameProgress;
+
   const onReadyFirstPlayer = (name) => {
-    setState((state) => {
+    setState(state => {
       state.firstPlayer.isReady = true;
       state.firstPlayer.name = name;
+      state.gameProgress.currentPlayer = name;
       state.step = 'prepareSecondPlayer';
     })
   };
 
   const onReadySecondPlayer = (name) => {
-    setState((state) => {
+    setState(state => {
       state.secondPlayer.isReady = true;
       state.secondPlayer.name = name;
       state.step = 'chooseFieldSize';
@@ -37,9 +46,59 @@ function App() {
   };
 
   const setSize = (size) => {
-    setState((state) => {
-      state.fieldSize = size
+    setState(state => {
+      state.fieldSize = size;
+      state.step = 'game';
     })
+  }
+
+  const checkWin = () => {
+    let combinations = [
+      '012',
+      '345',
+      '678',
+      '036',
+      '147',
+      '258',
+      '048',
+      '246',
+    ];
+    const firstResults = [...first];
+    const secondResults = [...second];
+    console.log('firstResults.sort().join :', firstResults.sort().join(''));
+    combinations.forEach((item) => {
+      if (item === firstResults.sort().join('')) {
+        console.log('firstWin');
+      };
+      if (item === secondResults.sort().join('')) {
+        console.log('secondWin');
+      };
+    });
+  }
+
+  const makeMove = (e) => {
+    const current = state.gameProgress.currentPlayer;
+    if (first.includes(e.target.id) || second.includes(e.target.id)) return null;
+    e.target.textContent = 'X';
+    if (current === state.firstPlayer.name) {
+      const movesList = [...state.gameProgress.first];
+      movesList.push(e.target.id);
+      setState(state => {
+        state.gameProgress.currentPlayer = state.secondPlayer.name;
+        state.gameProgress.first = movesList;
+      });
+
+    } else {
+      e.target.textContent = 'O';
+      setState(state => {
+        const movesList = [...state.gameProgress.second];
+        movesList.push(e.target.id);
+        state.gameProgress.currentPlayer = state.firstPlayer.name;
+        state.gameProgress.second = movesList;
+      });
+    }
+    checkWin();
+    console.log('state :', state);
   }
 
   switch(state.step) {
@@ -50,7 +109,7 @@ function App() {
     case 'chooseFieldSize':
       return (<ChooseField setSize={setSize} />)
     case 'game':
-      return (<GameField />)
+      return (<GameField size={state.fieldSize} makeMove={makeMove} currentPlayer={state.gameProgress.currentPlayer} />)
     default:
   };
 }
--- src/components/GameField/GameField.jsx
+++ src/components/GameField/GameField.jsx
@@ -1,27 +1,32 @@
 import './styles.css';
+import _ from 'lodash';
 
-function GameField() {
+function GameField(props) {
+  const {size, makeMove, currentPlayer} = props;
+
+  const handleClick = (e) => {
+    makeMove(e);
+  };
+  let counter = 0;
+
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
-      <table className='sss'>
-        <tbody>
-          <tr>
-            <td className="cell"></td>
-            <td className="cell"></td>
-            <td className="cell"></td>
-          </tr>
-          <tr>
-            <td className="cell"></td>
-            <td className="cell"></td>
-            <td className="cell"></td>
-          </tr>
-          <tr>
-            <td className="cell"></td>
-            <td className="cell"></td>
-            <td className="cell"></td>
-          </tr>
-        </tbody>
-        </table>
+    <div className="d-flex flex-column justify-content-center align-items-center h-100">
+      <div className="mb-4">Ходит игрок: {currentPlayer}</div>
+      <div className="d-flex justify-content-center align-items-center">
+        <table className='sss'>
+          <tbody>
+            {
+              _.fill(Array(size), 1).map(() => {
+                return <tr key={counter}>{_.fill(Array(size), 1).map(() => {
+                  const id = counter;
+                  counter++;
+                  return <td id={id} key={id} onClick={handleClick} className="cell"></td>
+                })}</tr>
+              })
+            }
+          </tbody>
+          </table>
+      </div>
     </div>
   )
 }
